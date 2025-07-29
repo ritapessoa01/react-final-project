a0a8b9446e3ce9243264cbd825b0380f8438fd08
ritapessoa01 <ritappessoa@gmail.com>
2025-07-29 20:54:38 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,24 +3,35 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/ritapessoa01" target="_blank">
-          Rita Pessoa
-        </a>
-        , it is{" "}
-        <a
-          href="https://github.com/ritapessoa01/react-final-project"
-          target="_blank"
-        >
-          open sourced
-        </a>{" "}
-        and hosted on{" "}
-        <a href="https://ritas-react-weather-app.netlify.app/" target="_blank">
-          Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/ritapessoa01"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Rita Pessoa
+          </a>
+          , it is{" "}
+          <a
+            href="https://github.com/ritapessoa01/react-final-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://ritas-react-weather-app.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
