eb5e6e7b9029323d2a5c54a6bd1fed1798c1359a
ritapessoa01 <ritappessoa@gmail.com>
2025-07-29 20:39:44 +0100

    added footer with links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/ritapessoa01" target="_blank">
+          Rita Pessoa
+        </a>
+        , it is{" "}
+        <a
+          href="https://github.com/ritapessoa01/react-final-project"
+          target="_blank"
+        >
+          open sourced
+        </a>{" "}
+        and hosted on{" "}
+        <a href="https://ritas-react-weather-app.netlify.app/" target="_blank">
+          Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
