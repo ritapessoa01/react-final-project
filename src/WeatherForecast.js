0d305e09ed7b9fe2ba12dda3c172ba3f092ae391
ritapessoa01 <ritappessoa@gmail.com>
2025-09-18 01:13:10 +0100

    Fixed issue

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./WeatherForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
-import { cleanup } from "@testing-library/react";
 
 export default function WeatherForecast(props) {
   let [ready, setReady] = useState(false);
